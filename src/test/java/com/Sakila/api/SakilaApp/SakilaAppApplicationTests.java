324595bbb521b9ad8ecc9d473255f987cbe9af98
Nur Luski Levi <BritishPirate@users.noreply.github.com>
2022-09-01 15:10:51 +0100

    Changed Jacoco to 0.8.8 to fix build issues

--- src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
+++ src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
@@ -1,13 +1,15 @@
 package com.Sakila.api.SakilaApp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SakilaAppApplicationTests {
-/*
+
 	@Test
 	void contextLoads() {
+        Assertions.assertEquals("1", "1");
 	}
-*/
+
 }
